e956c61a5e851ddee68a2917eb17cafba0788be4
dharmil-patel <dharmilpatel06@gmail.com>
2021-12-05 16:49:11 +0530

    dark/light mode added switch functionality added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './themes.js'
+
+const StyledApp = styled.div`
+  color : ${(props) => props.theme.fontColor}
+`;
 
 function App() {
+
+  let themeName = localStorage.getItem('themeName');
+  const [theme, setTheme] = useState(themeName);
+
+  const themeToggler = () => {
+    if(theme === 'light'){
+      setTheme('dark');
+      localStorage.setItem('themeName', 'dark');
+    }
+    else if(theme === 'dark'){
+      setTheme('light');
+      localStorage.setItem('themeName', 'light');
+    }
+    else{ //default mode is light so when toggle button click for first time local storage value is null,so set dark mode 
+      setTheme('dark');
+      localStorage.setItem('themeName', 'dark');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <StyledApp>
+             <button onClick={() => themeToggler()}>Click here to change theme</button>
+ </StyledApp>
+    </ThemeProvider>
+
+
   );
 }
 
